18e25df3e4b0fcf4b4a964f61ecfaa8246d225fd
bmpitzen <bmpitzen@icloud.com>
2018-08-07 17:14:13 -0400

    Initial commit. Working on problem 10.

--- homework.js
+++ homework.js
@@ -2,20 +2,50 @@
 // returns the sum of those numbers.
 
 function sum (numbers) {
-  console.log('numbers', numbers)
-  return 0
+  var num = 0
+  for (var x of numbers) {
+    num += x;
+  }
+  return num 
 }
 
 // 2. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average (numbers){
+  
+  if (numbers.length > 0) {
+    return sum(numbers)/numbers.length
+  }
+}
 
 // 3. Create a function called "intersection" that takes two arrays and
 // returns a new array that contains the elements found in both arrays.
 // The order they are in does not matter, but no duplicates should be
 // in the returned array, even if they were in the input.
+function intersection (a1 , a2) {
+  var newNums = []
+  for (var num1 of a1){
+    for (var num2 of a2){
+      if (num1 === num2){
+        newNums.push(num2)
+      }
+    }
+  }
+return newNums
+}
+
 
 // 4. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum (array){
+  var min = array[0]
+  for (var num of array){
+    if (num < min){
+    min = num
+    }
+  }
+  return min
+}
 
 // 5. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -41,23 +71,73 @@ function sum (numbers) {
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
 
+function selectionSort (array){
+  var output = []
+  var arrayCopy = array.slice(0)
+  var min
+  for (var i = 0; i < array.length; i++){
+     // find min number in array copy
+     min = minimum(arrayCopy)
+    // insert into output array
+    output.push(min)
+    // delete from orig array copy
+    arrayCopy.splice(arrayCopy.indexOf(min),1)
+  }
+  return output
+}
+
 // 6. Create a function called "createUser" that takes a name and a Date object
 // and returns an object with the keys "name" and "dob" (date of birth) with
 // those values.
+function createUser (name, date){
+  var user = {name: name,
+  dob: new Date(date)}
+  if (name == undefined){
+    return undefined
+  } else {
+    return user
+  }
+}
 
 // 7. Create a function called "calculateAge" that takes a user created from
 // createUser and a Date object considered the current date, and calculates the user's
 // age in years on that date. You can use your code from yesterday's homework.
 
+function calculateAge (user, Date){
+    date1 = user.dob
+    date2 = Date
+    var ms = (date2.getTime() - date1.getTime())
+    if (date1<=date2){
+        return Math.trunc(ms/(1000*60*60*24*365.25))
+    } else {
+        return
+    }
+  }
+
 // 8. Create a function called "addAge" that takes a user created from createUser
 // and a Date object and adds a new key on the user object, "age", with the age
 // in years the user was on that date.
+
+function addAge (user,Date){
+  user.age = calculateAge(user,Date)
+} 
 
 // 9. Create a function called "createUsers" that takes two arrays of equal
 // length, the first being a list of names and the second being a list of dates of
 // birth, and returns a new array of objects created from those original arrays.
 
+function createUsers (names,dates){
+  var users =[]
+  for(var i=0; i<names.length; i++){
+    var user = createUser(names[i], dates[i])
+    users.push(user)
+  }  
+  return users
+}
+
 // 10. Create a function called "averageAge" that takes an array of users and
 // a Date object and returns the average age in years of the users on that date.
 // You do not have to handle the situation in which the current date is before
 // a user's date of birth.
+
+function averageAge (users,Date)
